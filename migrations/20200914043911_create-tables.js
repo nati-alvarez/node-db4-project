afc1f4dc87629c88e78d6cfc14290f0aae04e401
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-14 18:19:00 -0400

    Added instructions table

--- migrations/20200914043911_create-tables.js
+++ migrations/20200914043911_create-tables.js
@@ -6,16 +6,21 @@ exports.up = function(knex) {
     }).createTable("ingredients", table=>{
         table.increments();
         table.string("ingredient_name").notNullable().unique();
+    }).createTable("instructions", table=>{
+        table.increments();
+        table.string("step", 128);
+        table.integer("recipe_id").unsigned().notNullable().references("id").inTable("recipes").onUpdate("CASCADE").onDelete("CASCADE");
     }).createTable("recipe_ingredients", table=>{
         table.integer("recipe_id").unsigned().notNullable().references("id").inTable("recipes").onUpdate("CASCADE").onDelete("CASCADE");
         table.integer("ingredient_id").unsigned().notNullable().references("id").inTable("ingredients").onUpdate("CASCADE").onDelete("CASCADE");
         table.float("quantity").notNullable();
         table.unique(["recipe_id", "ingredient_id"]);
-    });
+    })
 };
 
 exports.down = function(knex) {
   return knex.schema.dropTableIfExists("recipe_ingredients")
+  .dropTableIfExists("instructions")
   .dropTableIfExists("ingredients")
   .dropTableIfExists("recipes");
 };
